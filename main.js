378cdeb135ee16bb00274473a672fd4a881b4b65
Thomas Melcher <thomas@couchfunk.de>
2018-01-02 16:24:06 +0100

    added primitive operator selection

--- main.js
+++ main.js
@@ -1,6 +1,48 @@
 (() => {
 
 	const slideContainer = document.getElementById("selection");
+	const operators = {
+		attacker: [
+			"Ash",
+			"Blackbeard",
+			"Blitz",
+			"Buck",
+			"Capitao",
+			"Dokkaebi",
+			"Fuze",
+			"Glaz",
+			"Hibana",
+			"IQ",
+			"Jackal",
+			"Montagne",
+			"Sledge",
+			"Thatcher",
+			"Thermite",
+			"Twitch",
+			"Ying",
+			"Zofia",
+		],
+		defender: [
+			"Bandit",
+			"Castle",
+			"Caveira",
+			"Doc",
+			"Echo",
+			"Ela",
+			"Frost",
+			"Jager",
+			"Kapkan",
+			"Lesion",
+			"Mira",
+			"Mute",
+			"Pulse",
+			"Rook",
+			"Smoke",
+			"Tachanka",
+			"Valkyrie",
+			"Vigil",
+		]
+	};
 
 	function removeEventTarget(event) {
 		let target = event.currentTarget;
@@ -16,6 +58,15 @@
 		return slide;
 	}
 
+	function createOperator(side) {
+		let pool = operators[side];
+		let operator = pool[Math.floor(Math.random() * pool.length)];
+		let el = document.createElement("div");
+		el.innerText = operator;
+		el.classList.add("operator");
+		return el;
+	}
+
 	function removeCurrentSlide() {
 		slideContainer.querySelectorAll(".slide:not(.removed)")
 		              .forEach(el => el.classList.add("removed"));
@@ -24,6 +75,8 @@
 	function chooseOperator(event) {
 		let side = event.currentTarget.getAttribute("data-side");
 		let slide = createSlide(side);
+		let operator = createOperator(side);
+		slide.appendChild(operator);
 		removeCurrentSlide();
 		slideContainer.appendChild(slide);
 	}

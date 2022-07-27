ac5ca3a9aef4b762ebf32790e53af37f5cfa0e84
Tundratier <github@melchernetz.de>
2022-07-27 16:51:03 +0200

    added motd icon

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -4,13 +4,14 @@ import style from "./home.mod.scss";
 import roster from "../../icons/roster.svg";
 import queue from "../../icons/queue.svg";
 import random from "../../icons/random.svg";
+import motd from "../../icons/motd.svg";
 import {Icon} from "../icon/icon";
 
 export function Home() {
 	return <nav class={style.Home}>
 		<LinkButton label={"Random"} path={"/random"}><Icon icon={random}/></LinkButton>
 		<LinkButton label={"Playlist"} path={"/queue"}><Icon icon={queue}/></LinkButton>
-		<LinkButton label={"MOTD"} path={"/motd"}><Icon icon={queue}/></LinkButton>
+		<LinkButton label={"MOTD"} path={"/motd"}><Icon icon={motd}/></LinkButton>
 		<LinkButton label={"Roster"} path={"/settings"}><Icon icon={roster}/></LinkButton>
 	</nav>
 }
